7c87eff68c097b816375c49e18545f6604e6c1e9
taitus <sebastia.roig@gmail.com>
2020-12-29 12:14:42 +0100

    Allow update the status of notifications
    You can update the same "notifications" section that we allow you to update in "my
     account".
    This "unsubscribe" section differs from the "my account" section because we do not
    need to be logged in to update the status of the notifications.

--- app/controllers/unsubscribes_controller.rb
+++ app/controllers/unsubscribes_controller.rb
@@ -5,9 +5,20 @@ class UnsubscribesController < ApplicationController
   def edit
   end
 
+  def update
+    @user.update!(unsubscribe_params)
+    redirect_to edit_unsubscribe_path, notice: t("flash.actions.save_changes.notice")
+  end
+
   private
 
     def set_user
       @user = User.find_by(unsubscribe_hash: params[:unsubscribe_hash])
+    end
+
+    def unsubscribe_params
+      attributes = [:email_on_comment, :email_on_comment_reply, :email_on_direct_message,
+                    :email_digest, :newsletter]
+      params.require(:user).permit(*attributes)
     end
 end
--- spec/system/unsubscribes_spec.rb
+++ spec/system/unsubscribes_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 describe "Unsubscribes" do
+  let!(:user) { create(:user) }
+
   scenario "Edit page" do
     visit edit_unsubscribe_path(user.unsubscribe_hash)
 
@@ -10,5 +12,30 @@ describe "Unsubscribes" do
     expect(page).to have_content "Receive by email website relevant information"
     expect(page).to have_content "Receive a summary of proposal notifications"
     expect(page).to have_content "Receive emails about direct messages"
+  end
+
+  context "Update" do
+    scenario "Allow update the status notification" do
+      user.update!(email_on_comment: true,
+                   email_on_comment_reply: true,
+                   email_on_direct_message: true,
+                   email_digest: true,
+                   newsletter: true)
+      visit edit_unsubscribe_path(user.unsubscribe_hash)
+
+      uncheck "Notify me by email when someone comments on my proposals or debates"
+      uncheck "Notify me by email when someone replies to my comments"
+      uncheck "Receive by email website relevant information"
+      uncheck "Receive a summary of proposal notifications"
+      uncheck "Receive emails about direct messages"
+      click_button "Save changes"
+
+      expect(page).to have_content "Changes saved"
+      expect(user.reload.email_on_comment).to eq false
+      expect(user.reload.email_on_comment_reply).to eq false
+      expect(user.reload.email_on_direct_message).to eq false
+      expect(user.reload.email_digest).to eq false
+      expect(user.reload.newsletter).to eq false
+    end
   end
 end
