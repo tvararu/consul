b691be3e3b8c180a5408295f0daff264738ec8eb
Javi Martín <javim@elretirao.net>
2021-06-18 02:42:38 +0200

    Add SDG English icons in SVG format
    So we provide an alternative to the PNG images. SVG icons are smaller,
    can be compressed with gzip, and are scalable.
    
    These icons have been downloaded from https://globalgoals.org.
    Unfortunately I haven't found SVG files for languages other than
    English; on that page they emulate icons in other languages by combining
    different SVG files inside the HTML and styling them with CSS so they
    look like an icon, but they don't provide fully translated SVG files.
    
    Note we're loading them using `<use>` instead of using separate files
    and loading them as images. We're doing so because, when compressed, the
    size of the icons is very small (1KB-3KB), and having one request to get
    them all (33KB compressed) makes the page load faster. With PNGs we
    aren't using a sprite because rendering them in an `<img>` tag would be
    harder and the resulting PNG sprite would be too big (about 250KB).
    
    We could also have loaded them inline. However, in this case the icons
    would not be cached nor compressed, and they might appear several times
    on the same page, making the generated HTML much larger.
    
    Also note we're using the `img` role with an `aria-label` attribute.
    This was the only way I found to make my screen reader (Orca) correctly
    announce the text when the icon is used inside a link. Using a `<title>`
    element inside the SVG didn't work. Regarding this topic, the
    documentation on the Mozilla Development Network says [1]:
    
    > If you are using embedded SVG images in your page, it is a good idea
    > to set role="img" on the outer <svg> element and give it a label. This
    > will cause screen readers to just consider it as a single entity and
    > describe it using the label, rather than trying to read out all the
    > child nodes
    
    Finally, note that, for reasons I'm not sure about, we need to specify a
    viewBox in the SVG element. Otherwise the link will be slightly bigger
    than the SVG inside it, which will produce a strange effect when
    focusing on the link.
    
    [1] https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/Role_Img#svg_and_roleimg

--- app/components/sdg/goals/icon_component.rb
+++ app/components/sdg/goals/icon_component.rb
@@ -6,8 +6,18 @@ class SDG::Goals::IconComponent < ApplicationComponent
     @goal = goal
   end
 
+  def png
+    image_tag image_path, alt: image_text, class: "sdg-goal-icon"
+  end
+
   def image_path
-    "sdg/#{locale}/goal_#{code}.png"
+    png_path(locale)
+  end
+
+  def svg
+    tag.svg(role: "img", "aria-label": image_text, viewBox: "0 0 1000 1000", class: "sdg-goal-icon") do
+      tag.use(href: asset_path("#{svg_path(locale)}#goal_#{goal.code}"))
+    end
   end
 
   private
@@ -17,8 +27,20 @@ class SDG::Goals::IconComponent < ApplicationComponent
     end
 
     def locale
-      [*I18n.fallbacks[I18n.locale], "default"].find do |fallback|
-        AssetFinder.find_asset("sdg/#{fallback}/goal_#{code}.png")
+      @locale ||= [*I18n.fallbacks[I18n.locale], "default"].find do |fallback|
+        AssetFinder.find_asset(svg_path(fallback)) || AssetFinder.find_asset(png_path(fallback))
       end
+    end
+
+    def svg_available?
+      AssetFinder.find_asset("sdg/#{locale}/goals.svg")
+    end
+
+    def svg_path(locale)
+      "sdg/#{locale}/goals.svg"
+    end
+
+    def png_path(locale)
+      "sdg/#{locale}/goal_#{code}.png"
     end
 end
