de5cd827281859f0e9ce4254a0cb17afa8a2b8d5
tsundokul <42388638+tsundokul@users.noreply.github.com>
2021-08-12 21:09:36 +0300

    Move CSS and i18n changes to proper location, add logo (#12)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,5 +72,8 @@ Rails.application.configure do
   end
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::FileUpdateChecker
+
+  # Disable fingerprinting for dev to avoid server restarting on CSS changes
+  config.assets.digest = false
 end
